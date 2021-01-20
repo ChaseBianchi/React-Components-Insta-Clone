a63428db23fc0fc5c7e07d779605409e48c68144
ChaseBianchi <73509539+ChaseBianchi@users.noreply.github.com>
2021-01-20 00:14:01 -0500

    Apps.js

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ import SearchBar from '../src/components/SearchBar/SearchBar';
 import dummyData from '../src/dummy-data';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
-import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  const [array, setArray] = useState(dummyData);
+  const [posts, setPosts] = useState(dummyData);
+  const [search, setSearch] =useState('')
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -30,6 +30,9 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(obj =>{
+      return {...obj, obj.likes=obj.likes + 1}
+    })
   };
 
   return (
